3526638f96447edc6643337e5030b1ce367acf49
rafael <rafaelbarroslimaa@gmail.com>
2021-02-01 22:40:40 -0300

    add arquivos para refatoracao do codigo

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+  express: server
+})
+
+server.get("/", function(req, res) {
+  server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+  return res.render("about")
+})
+
+server.get("/portfolio", function(req, res) {
+  server.use(function(req, res) {
+    res.status(404).render("not-found");
+  });
+  return res.render("portfolio")
+})
+
+server.listen(5000, function() {
+  console.log("Server is running.")
+})
